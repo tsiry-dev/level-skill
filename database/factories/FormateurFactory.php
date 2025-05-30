0e17de84b6252a8078f17a0547eb198fa6635ce6
Tsiry <andriamiarijaonatsiri@gmail.com>
2025-05-30 18:43:16 +0300

    Migration Niveaux

--- database/factories/FormateurFactory.php
+++ database/factories/FormateurFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Formateur>
@@ -16,8 +17,11 @@ class FormateurFactory extends Factory
      */
     public function definition(): array
     {
+        $name = fake()->name();
         return [
-            //
+            'name' => $name,
+            'slug' => Str::slug($name) . '-'. time() . mt_rand(200, 1500),
+            'profile' => fake()->text(),
         ];
     }
 }
