7369b4edfac4ce0c68a8a4abc250be942a08b464
Tsiry <andriamiarijaonatsiri@gmail.com>
2025-05-30 18:36:07 +0300

    Migration formateurs and related by user(students)

---
+++ app/Models/Formateur.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Formateur extends Model
+{
+    /** @use HasFactory<\Database\Factories\FormateurFactory> */
+    use HasFactory;
+}
--- resources/js/pages/auth/Login.vue
+++ resources/js/pages/auth/Login.vue
@@ -29,7 +29,7 @@ const submit = () => {
 
 <template>
     <AuthBase title="Log in to your account" description="Enter your email and password below to log in">
-        <Head title="Log in" />
+        <Head title="Conexion" />
 
         <div v-if="status" class="mb-4 text-center text-sm font-medium text-green-600">
             {{ status }}
